edd4ed1318bef8b524a7d7e7efd30fcc53cfc8b0
Kerim Satirli <k.satirli@gmail.com>
2022-12-06 05:45:20 +0100

    bumps GitHub from `5.3.0` to `5.11.0`, and TFE from `0.37.0` to `0.39.0`

--- terraform.tf
+++ terraform.tf
@@ -12,16 +12,16 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/integrations/github/5.3.0/
+    # see https://registry.terraform.io/providers/integrations/github/5.11.0/
     github = {
       source  = "integrations/github"
-      version = ">= 5.3.0, < 6.0.0"
+      version = ">= 5.11.0, < 6.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/tfe/0.37.0/
+    # see https://registry.terraform.io/providers/hashicorp/tfe/0.39.0/
     tfe = {
       source  = "hashicorp/tfe"
-      version = ">= 0.37.0, < 1.0.0"
+      version = ">= 0.39.0, < 1.0.0"
     }
   }
 
