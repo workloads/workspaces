8068ca1dd0123e7241a8800a5f03c44d275de361
Kerim Satirli <k.satirli@gmail.com>
2023-03-20 12:45:59 +0100

    work-up for policies

--- terraform_cloud_teams.tf
+++ terraform_cloud_teams.tf
@@ -2,6 +2,18 @@
 resource "tfe_team" "owners" {
   name         = "owners"
   organization = var.tfe_organization_name
+  visibility   = "organization"
+
+  organization_access {
+    manage_policies         = true
+    manage_policy_overrides = true
+    manage_workspaces       = true
+    manage_vcs_settings     = true
+    manage_providers        = true
+    manage_modules          = false
+    manage_run_tasks        = true
+    manage_projects         = true
+  }
 }
 
 # see https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/team_member
