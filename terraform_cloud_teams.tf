ca21eeb289e2ffb70589cad6a5d8f8862b26ae6f
Kerim Satirli <k.satirli@gmail.com>
2023-04-14 11:25:01 +0200

    adds `viewers` team

--- terraform_cloud_teams.tf
+++ terraform_cloud_teams.tf
@@ -5,7 +5,7 @@ resource "tfe_team" "owners" {
   organization = var.tfe_organization_name
 
   # owners are generally kept secret as they present an attack vector
-  visibility   = "secret"
+  visibility = "secret"
 
   # see https://developer.hashicorp.com/terraform/cloud-docs/users-teams-organizations/permissions#organization-permissions
   organization_access {
@@ -35,3 +35,25 @@ resource "tfe_team_member" "owners" {
   username = each.value.username
 }
 
+# The `owners` team is a pre-existing resource and must be imported before it can be used through the `tfe` provider.
+# see https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/team
+resource "tfe_team" "viewers" {
+  name         = "viewers"
+  organization = var.tfe_organization_name
+  visibility   = "organization"
+
+  # see https://developer.hashicorp.com/terraform/cloud-docs/users-teams-organizations/permissions#organization-permissions
+  organization_access {
+    manage_policies         = false
+    manage_policy_overrides = false
+    manage_workspaces       = false
+    manage_vcs_settings     = false
+    manage_providers        = false
+    manage_modules          = false
+    manage_run_tasks        = false
+    manage_projects         = false
+
+    read_projects   = true
+    read_workspaces = true
+  }
+}
