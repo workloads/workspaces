c9b09f4f8b05322b5cc1a781eafd865ee153099b
Kerim Satirli <k.satirli@gmail.com>
2023-09-10 18:26:46 +0200

    updates Workspaces to Terraform 1.5.7

--- variables.tf
+++ variables.tf
@@ -401,7 +401,9 @@ variable "tfe_workspace_auto_apply" {
 variable "tfe_workspace_terraform_version" {
   type        = string
   description = "Terraform version to use for this Workspace."
-  default     = "1.5.5"
+
+  # see https://releases.hashicorp.com/terraform/
+  default = "1.5.7"
 }
 
 locals {
@@ -467,6 +469,10 @@ locals {
       description = "Datadog App Key."
       sensitive   = true
     },
+  ]
+
+  digitalocean_variables = [
+
   ]
 
   discord_variables = [
