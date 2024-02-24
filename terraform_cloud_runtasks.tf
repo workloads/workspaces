90d482dd3ed20a5fd00a8fabcc3afe11c194502a
Kerim Satirli <k.satirli@gmail.com>
2024-02-24 09:20:18 -0500

    disable Infracost Run Task

--- terraform_cloud_runtasks.tf
+++ terraform_cloud_runtasks.tf
@@ -1,7 +1,7 @@
 # see https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/organization_run_task
 resource "tfe_organization_run_task" "infracost" {
   description  = "Infracost Integration (Org ID: `${var.infracost_org}`)."
-  enabled      = true
+  enabled      = false
   hmac_key     = var.infracost_runtask_hmac_key
   name         = "infracost"
   organization = tfe_organization.main.name
